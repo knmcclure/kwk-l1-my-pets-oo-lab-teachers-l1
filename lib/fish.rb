ef2506d98577faa557a9e3cb7cdb6d2ab64f9edd
Kyla McClure <kyla.mcclure.16@tfacorps.org>
2018-07-27 02:50:00 +0000

    automatically backed up by learn

--- lib/fish.rb
+++ lib/fish.rb
@@ -1,3 +1,22 @@
 class Fish
   # code goes here
+  def initialize(name)
+    @name = name
+    @mood = "nervous"
+  end
+
+  def mood=(mood)
+    @mood = mood
+  end
+
+  def mood
+    @mood
+  end
+
+  def name
+    @name
+  end
 end
+
+bubbles = Fish.new("bubbles")
+puts bubbles.mood
--- lib/owner.rb
+++ lib/owner.rb
@@ -1,4 +1,52 @@
+require_relative 'fish'
+
 class Owner
   # code goes here
+  def initialize(species)
+    @species = species
+    @pets = {:fishes => [], :dogs => [], :cats => []}
+  end
 
- end
+  def pets
+    @pets
+  end
+
+  def buy_fish(fish)
+    @pets[:fishes] << fish
+  end
+
+  def buy_cat(cat)
+    @pets[:cats] << cat
+  end
+
+  def buy_dog(dog)
+    @pets[:dogs] << dog
+  end
+  def species
+    @species
+  end
+
+  def say_species
+    "I am a #{@species}."
+  end
+
+  def name=(name)
+    @name = name
+  end
+
+  def name
+    @name
+  end
+
+  def list_pets
+    "I have #{@pets[:fishes].count} fish, #{@pets[:dogs].count} dog(s), and #{@pets[:cats].count} cat(s)."
+  end
+
+
+end
+
+owner = Owner.new("human")
+puts owner.list_pets
+
+bubbles = Fish.new("bubbles")
+puts bubbles.name
